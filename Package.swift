a56242b8c8cff31e702db969845edc069056eb52
James Ruston <jruston90@gmail.com>
2020-07-03 13:43:30 +0100

    uppercase B in name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "bonsai",
+    name: "Bonsai",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "bonsai",
-            targets: ["bonsai"]),
+            name: "Bonsai",
+            targets: ["Bonsai"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "bonsai",
+            name: "Bonsai",
             dependencies: [],
             path: "Classes"),
         .testTarget(
-            name: "bonsaiTests",
-            dependencies: ["bonsai"]),
+            name: "BonsaiTests",
+            dependencies: ["Bonsai"]),
     ]
 )
