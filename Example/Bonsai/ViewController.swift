02df672f1c9db17c1737f5153e6ab64e34a2a4eb
John Crossley <jonnysnip3r@gmail.com>
2021-10-26 16:01:49 +0100

    Log dictionaries and more goodies
    - Can now log dictionaries
    - Can now log errors
    - Can now pass dictionary parameters to drivers for the user of things like crashlytics, persist to disk etc.
    - Bumped pod version
    
    Co-Authored-By: James Ruston <1382570+jamesruston@users.noreply.github.com>

--- Example/Bonsai/ViewController.swift
+++ Example/Bonsai/ViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 
+enum MyBonsaiError: Swift.Error {
+    case networkFailure
+    case generic(String)
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Logger.log(.verbose, "verbose")
-        "TESTING".log(.verbose)
-        // Do any additional setup after loading the view, typically from a nib.
+        demoDefaultLogging()
+        demoDictionaryLogging()
+        demoErrorLogging()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func demoDefaultLogging() {
+        Logger.log(.verbose, "verbose")
+        "TESTING".log(.verbose)
+    }
 
+    private func demoDictionaryLogging() {
+        ["name": "Henry", "age": 6].log()
+        [1: "The first item", 2: "The second item"].log()
+        ["names": ["Mr. Orange", "Mr. Blonde", "Nice guy Eddie", "Mr. Pink", "Mr. Blue"]].log()
+    }
+    
+    private func demoErrorLogging() {
+        MyBonsaiError.networkFailure.log(.warning)
+        MyBonsaiError.generic("Asset fetching error").log(.warning)
+    }
 }
-
